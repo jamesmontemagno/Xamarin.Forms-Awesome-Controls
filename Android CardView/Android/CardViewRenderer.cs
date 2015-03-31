82ea6f424451f5bdf221bdbc6c2c44cec29dcee7
Daniel Hindrikes <daniel@hindrikes.se>
2015-03-31 12:52:19 +0200

    android renderer for CardContentView

--- Android CardView/Android/CardViewRenderer.cs
+++ Android CardView/Android/CardViewRenderer.cs
@@ -5,120 +5,52 @@ using Android.Views;
 using CardViewForms;
 using Android.Support.V7.Widget;
 using CardViewFormsAndroid;
+using Android.Widget;
 
 [assembly:ExportRendererAttribute(typeof(CardContentView), typeof(CardViewRenderer))]
 namespace CardViewFormsAndroid
 {
-	public class CardViewRenderer : CardView, 
-		IVisualElementRenderer
-	{
-		public CardViewRenderer () : base (Forms.Context)
-		{
-		}
+    public class CardViewRenderer : ViewRenderer<CardContentView, CardView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<CardContentView> e)
+        {
+            base.OnElementChanged(e);
 
-		public event EventHandler<VisualElementChangedEventArgs> ElementChanged;
+            if (Control == null)
+            {
+                var control = new CardView(Forms.Context);
 
-		bool init;
-		ViewGroup packed;
-		public void SetElement (VisualElement element)
-		{
-			var oldElement = this.Element;
+                SetNativeControl(control);
+                var packager = new VisualElementPackager(this);
+                packager.Load();
+            }
+        }
 
-			if (oldElement != null)
-				oldElement.PropertyChanged -= HandlePropertyChanged;
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
 
-			this.Element = element;
-			if (this.Element != null) {
-				UpdateContent ();
-				this.Element.PropertyChanged += HandlePropertyChanged;
-			}
+            if (Control != null)
+            {
 
-			if (!init) {
-				init = true;
-				//sizes to match the forms view
-				//updates properties, handles visual element properties
-				Tracker = new VisualElementTracker (this);
-				//add and remove children automatically
-				//this is broken right now so I do it manually
-				//packager = new VisualElementPackager (this);
-				//packager.Load ();
+                if (e.PropertyName == "Padding" || e.PropertyName == "Renderer")
+                {
+                    Control.SetPadding((int)Element.Padding.Left, (int)Element.Padding.Top, (int)Element.Padding.Right, (int)Element.Padding.Bottom);
+                }
 
-				SetContentPadding ((int)TheView.Padding.Left, (int)TheView.Padding.Top,
-					(int)TheView.Padding.Right, (int)TheView.Padding.Bottom);
+                if (e.PropertyName == "CornderRadius" || e.PropertyName == "Renderer")
+                {
+                    Control.Radius = Element.CornderRadius;
+                }
 
-				Radius = TheView.CornderRadius;
-			}
+            }
+        }
 
-			if(ElementChanged != null)
-				ElementChanged (this, new VisualElementChangedEventArgs (oldElement, this.Element));
-		}
 
-		private void UpdateContent()
-		{
-			if (TheView.Content == null)
-				return;
 
-			if (packed != null)
-				RemoveView (packed);
+    }
 
-			var child = RendererFactory.GetRenderer (TheView.Content);
-			packed = child.ViewGroup;
-			AddView (packed);
-		}
 
-	
-
-		public CardContentView TheView
-		{
-			get { return this.Element == null ? null : (CardContentView)Element; }
-		}
-			
-
-		void HandlePropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == "Content") {
-				UpdateContent ();
-			} else if (e.PropertyName == CardContentView.PaddingProperty.PropertyName) {
-				SetContentPadding ((int)TheView.Padding.Left, (int)TheView.Padding.Top,
-					(int)TheView.Padding.Right, (int)TheView.Padding.Bottom);
-			} else if (e.PropertyName == CardContentView.CornerRadiusProperty.PropertyName) {
-				this.Radius = TheView.CornderRadius;
-			} else if (e.PropertyName == CardContentView.BackgroundColorProperty.PropertyName) {
-				//if (TheView.BackgroundColor != null)
-				//	SetBackgroundColor (TheView.BackgroundColor.ToAndroid ());
-
-			}
-		}
-
-		public SizeRequest GetDesiredSize (int widthConstraint, int heightConstraint)
-		{
-			packed.Measure (widthConstraint, heightConstraint);
-
-			//Measure child here and determine size
-			return new SizeRequest (new Size (packed.MeasuredWidth, packed.MeasuredHeight));
-		}
-
-		public void UpdateLayout ()
-		{
-			if (Tracker == null)
-				return;
-
-			Tracker.UpdateLayout ();
-		}
-
-		public VisualElementTracker Tracker {
-			get;
-			private set;
-		}
-
-		public Android.Views.ViewGroup ViewGroup {
-			get{ return this; }
-		}
-
-		public VisualElement Element {
-			get;
-			private set;
-		}
-	}
+   
 }
 
